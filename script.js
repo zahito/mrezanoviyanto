5cf6f8cf225935aa9c407d586c9fcf298bd08a7d
Muhammad Reza Noviyanto <52406100+zahito@users.noreply.github.com>
2021-01-12 15:44:49 +0700

    update tampilan

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Tech Support.", "Gamers."],
+        strings: ["Tech Support.","Technician.","Gamers."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Tech Support.", "Gamers."],
+        strings: ["Tech Support.","Technician.","Gamers."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
